ed5e74cd48fecee5cb54fbec7972a68a636ce708
Ankita <ankietathorve@gmail.com>
2026-04-09 12:44:31 +0530

    Create variable to use elements

---
+++ script.js
@@ -0,0 +1,7 @@
+const storyContainer = document.querySelector(".story-container");
+
+const scaryStroryBtn = document.getElementById("scary-btn");
+const funnyStroryBtn = document.getElementById("funny-btn");
+const adventureStroryBtn = document.getElementById("adventure-btn");
+
+const resultParagraph = document.querySelector("#result");
